d479bbe3379e46b0440895e4b81d01f709a492e8
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-04-17 17:50:53 +0530

    easyQuesiton.js

--- 06_Array_Question/01_striver/02_day/easyQuesiton.js
+++ 06_Array_Question/01_striver/02_day/easyQuesiton.js
@@ -7,6 +7,9 @@ const moveNumbersToLeft = (arr) => {
     return arr
 }
 
+
+// brute method to move element
+
 const moveNumbersToLeftnNumbersBruteForce = (arr, k) => {
     const n = arr.length
     k = k % n
@@ -24,9 +27,92 @@ const moveNumbersToLeftnNumbersBruteForce = (arr, k) => {
     return arr
 }
 
+
+// better approch to do this quesiton
+
+const oneByOne = (arr) => {
+    const first = arr[0];
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    arr[arr.length - 1] = first;
+}
+const moveNumbersToLeftnNumbersBetterApproch = (arr, k) => {
+    const n = arr.length;
+    k = k % n; // Handle k > n
+
+    for (let i = 0; i < k; i++) {
+        oneByOne(arr)
+    }
+
+    return arr;
+}
+
+// Zeros to last 
+
+const zerosToLastBrute = (arr) => {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            newArr.push(arr[i])
+        }
+    }
+    while (newArr.length < arr.length) {
+        newArr.push(0)
+    }
+    return newArr;
+}
+const zerosToLastBetterApproch = (arr) => {
+    let j = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[j] = arr[i]
+            j++
+        }
+    }
+    while (j < arr.length) {
+        arr[j] = 0
+        j++
+    }
+    return arr;
+}
+const zeroOptiomalApproch = (arr) => {
+    let j = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            if (i !== j) {
+                [arr[i], arr[j]] = [arr[j], arr[i]]
+            }
+            j++
+        }
+    }
+    return arr
+}
+
+const zeroOptiomalApproch2 = (arr) => {
+    let i = 0
+    let j = 1
+    while (j < arr.length) {
+        if (arr[i] === 0 && arr[j] !== 0) {
+            arr[i] = arr[j]
+            arr[j] = 0
+            i++
+        } if (arr[i] !== 0) {
+            i++
+        }
+        j++
+    }
+    return arr
+}
+
 const prompt = require('prompt-sync')()
 const input = prompt(`Enter numbers with space: `)
 const k = prompt(`Enter the value of rotation: `)
 const arr = input.split(' ').map(Number)
+console.log(zeroOptiomalApproch2(arr))
+console.log(zeroOptiomalApproch(arr))
+console.log(zerosToLastBetterApproch(arr))
+console.log(zerosToLastBrute(arr))
+console.log(moveNumbersToLeftnNumbersBetterApproch(arr, k))
 console.log(moveNumbersToLeftnNumbersBruteForce(arr, k))
-// console.log(moveNumbersToLeft(arr))
+console.log(moveNumbersToLeft(arr))
